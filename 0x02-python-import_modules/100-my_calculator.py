b26c3abf3ea4c3771b14b88497c5b5e04a114b39
Anas TAHER <taheranas24@gmail.com>
2023-06-09 13:06:59 +0200

    Task 100 commit

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+import sys
+import calculator_1
+
+
+def main():
+    argc = len(sys.argv) - 1
+    if argc != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+
+    a = int(sys.argv[1])
+    operator = sys.argv[2]
+    b = int(sys.argv[3])
+
+    if operator == '+':
+        result = calculator_1.add(a, b)
+    elif operator == '-':
+        result = calculator_1.sub(a, b)
+    elif operator == '*':
+        result = calculator_1.mul(a, b)
+    elif operator == '/':
+        result = calculator_1.div(a, b)
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+
+    print("{} {} {} = {}".format(a, operator, b, result))
+
+
+if __name__ == "__main__":
+    main()
