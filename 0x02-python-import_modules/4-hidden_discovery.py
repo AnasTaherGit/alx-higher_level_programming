6f7f177f9889fd8ee3f7675bca34c4ea51d8dea1
Anas TAHER <taheranas24@gmail.com>
2023-06-09 12:33:07 +0200

    Task 4 commit

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import importlib
+import pkgutil
+
+
+def main():
+    module = importlib.import_module('hidden_4')
+    names = list(filter(lambda name: not name.startswith("__"), dir(module)))
+    names.sort()
+    for name in names:
+        print(name)
+
+
+if __name__ == "__main__":
+    main()
