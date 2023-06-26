31f2493724b1285fc475d3bdc60cf724b661ecea
Anas TAHER <taheranas24@gmail.com>
2023-06-26 23:14:54 +0200

    Task 1 fix

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-
 def safe_print_integer(value):
     try:
         assert isinstance(value, int)
         print("{:d}".format(value))
         return True
-    except:
+    except AssertionError:
         return False
