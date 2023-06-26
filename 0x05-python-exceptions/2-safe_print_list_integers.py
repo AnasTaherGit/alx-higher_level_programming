10b57373abcd597f3a7eb803782affd39dc849d7
Anas TAHER <taheranas24@gmail.com>
2023-06-26 23:28:41 +0200

    Task 2 fix : Type Error exception

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     nb_print = 0
-    for i in range(x):
-        try:
-            assert isinstance(my_list[i], int)
-            print("{:d}".format(my_list[i]), end="")
-            nb_print += 1
-        except AssertionError:
-            continue
-        except IndexError:
-            break
-    print()
+    try:
+        for i in range(x):
+            try:
+                assert isinstance(my_list[i], int)
+                print("{:d}".format(my_list[i]), end="")
+                nb_print += 1
+            except AssertionError:
+                continue
+            except IndexError:
+                break
+        print()
+    except TypeError:
+        pass
 
     return nb_print
